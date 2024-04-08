f8ffd344af2b9296b22e07fd7b1823a2ccbad02a
Hirotonomi <asztonpsiak@gmail.com>
2024-04-08 09:40:33 +0200

    anything kinda doing somthing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { FormComponent } from './form/form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: FormComponent
+    }
+    
+
+];
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css'
+})
+export class FormComponent {
+  color = new FormControl('');
+
+}
